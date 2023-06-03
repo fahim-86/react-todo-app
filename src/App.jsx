e29453fb8b00b4b6a3a3bcd93db17ae11135de28
fahim-86 <famambission491@gmail.com>
2023-06-04 04:20:31 +1000

    Add form onsubmit function and prepare data to render

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,27 @@ import "./styles.css"
 
 export default function App() {
 
-  const [newItem, setNewItem] = useState();
+  //~~newItem~~ is variable that holds new data and renders it to html but immutable
+  //~~setNewItem~~ is a method that used to send data to ~~newItem~~
+  const [newItem, setNewItem] = useState("");
+  const [toDos, setToDo] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    //~~...~~ spread operator helps chreating a new copy of an array "...arrayName"
+    setToDo(currentToDo => {
+      return [
+        ...currentToDo, { id: crypto.randomUUID(), title: newItem, completed: false}
+      ]
+    });
+
+    console.log(toDos);
+  }
 
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
           <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
@@ -17,6 +33,13 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox"/>
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
         <li>
           <label>
             <input type="checkbox"/>
