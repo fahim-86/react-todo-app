a40a1907e76e6fa92117d5c6b0501086ac514858
fahim-86 <famambission491@gmail.com>
2023-06-04 00:07:21 +1000

    Setup input data value using useState function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react"
 import "./styles.css"
 
 
 export default function App() {
+
+  const [newItem, setNewItem] = useState();
+
   return (
     <>
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item"/>
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
         </div>
         <button className="btn">Add</button>
       </form>
@@ -15,10 +19,10 @@ export default function App() {
       <ul className="list">
         <li>
           <label>
-            <input type="checkbox"></input>
+            <input type="checkbox"/>
             Item 1
           </label>
-
+          <button className="btn btn-danger">Delete</button>
         </li>
       </ul>
     </>
